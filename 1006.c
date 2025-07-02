ea5e34d4f671a827a24ac5163db4edc8ec00b70e
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-02 10:35:18 -0300

    Update 1006.c

--- 1006.c
+++ 1006.c
@@ -3,10 +3,10 @@
 int main() {
  float A, B, C, MEDIA;
  scanf("%f", &A);
-scanf("%f", &B); 
-scanf("%f", &C);
+ scanf("%f", &B); 
+ scanf("%f", &C);
  MEDIA = (2*A + 3*B + C*5)/10;
  printf("MEDIA = %.1f\n", MEDIA);
 
-    return 0;
+return 0;
 }
