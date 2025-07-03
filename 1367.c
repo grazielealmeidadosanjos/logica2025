a09980401a8bd34b99df9ced5c799c5350034240
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-03 20:06:00 -0300

    Update 1367.c

--- 1367.c
+++ 1367.c
@@ -13,9 +13,10 @@ int main() {
     char X, Y[10];
 
     scanf("%d", &submissionCount);
-    if (submissionCount == 0){
+    if (submissionCount == 0) {
         return 0;
-}
+    }
+
     Problem problems[MAX_PROBLEMS];
     for (int i = 0; i < MAX_PROBLEMS; i++) {
         problems[i].penalty = 0;
@@ -25,32 +26,24 @@ int main() {
     int totalTime = 0;
     int totalSolved = 0;
 
- 
-
     for (int i = 0; i < submissionCount; i++) {
         scanf(" %c", &X);
         scanf("%d", &T);
         scanf("%s", Y);
-        //printf("%c %d %s", X,T,Y);
-        //printf("%d\n",problems[X].penalty);
-        
-        
-        if(strcmp(Y, "correct")== 0){
-            problems[X].solved = T+problems[X].solved;
-            totalSolved = totalSolved+1;
-            totalTime = totalTime+problems[X].solved+problems[X].penalty;
+        // printf("%c %d %s", X, T, Y);
+        // printf("%d\n", problems[X].penalty);
+
+        if (strcmp(Y, "correct") == 0) {
+            problems[X].solved = T + problems[X].solved;
+            totalSolved = totalSolved + 1;
+            totalTime = totalTime + problems[X].solved + problems[X].penalty;
         }
-        if((strcmp(Y, "incorrect")== 0)){
-        problems[X].penalty = problems[X].penalty+ 20;
+
+        if (strcmp(Y, "incorrect") == 0) {
+            problems[X].penalty = problems[X].penalty + 20;
         }
-        
-    
-        
-        
-        
- 
-    //printf(" %c, %d, %d\n", X,problems[X].solved, problems[X].penalty);
-        
+
+        // printf(" %c, %d, %d\n", X, problems[X].solved, problems[X].penalty);
     }
 
     printf("%d %d\n", totalSolved, totalTime);
