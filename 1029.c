355aceb533e237f62a9219bfd67b84cc016e1d4d
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-03 20:18:46 -0300

    Update 1029.c

--- 1029.c
+++ 1029.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-int calls=0 ;
 
-int fib(int n){
+int calls = 0;
+
+int fib(int n) {
     calls++;
-    if (n==0) return 0;
-    if (n==1) return 1;
-    return fib(n-1) + fib (n-2);
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib(n - 1) + fib(n - 2);
 }
 
 int main() {
-    int n, N, i; 
+    int n, N, i;
     scanf("%d", &N);
-    for(i=N; i>0; i--){
+    for (i = N; i > 0; i--) {
         scanf("%d", &n);
         calls = -1;
-        printf("fib(%d) = %d calls = %d", n, calls, fib(n));
+        printf("fib(%d) = %d calls = %d\n", n, fib(n), calls);
     }
     return 0;
 }
+
