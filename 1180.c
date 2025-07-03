773f0e11ccd0e192879b8b60b587730471de9da8
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-03 20:14:41 -0300

    Update 1180.c

--- 1180.c
+++ 1180.c
@@ -5,22 +5,22 @@ int main() {
     int x[1000];
 
     scanf("%d", &n);
-    
-    for(i = 0; i < n; i++) {
+
+    for (i = 0; i < n; i++) {
         scanf("%d", &x[i]);
-        if(i == 0) {
+        if (i == 0) {
             menor = x[i];
             pos = i;
         } else {
-            if(x[i] < menor) {
+            if (x[i] < menor) {
                 menor = x[i];
                 pos = i;
             }
         }
     }
 
-     printf("Menor valor: %d\n", menor);
-     printf("Posicao: %d\n", pos);
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", pos);
 
-     return 0;
+    return 0;
 }
